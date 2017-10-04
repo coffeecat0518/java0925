2f8da863ca553b2aaa35e296cc963c8c97dfd15f
coffeecat0518 <106022415@gmail.com>
2017-10-04 08:56:17 +0800

    第一次作業

--- src/成績.java
+++ src/成績.java
@@ -2,16 +2,15 @@ public class 成績 {
     public  static void main (String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
-        System.out.println("請輸入三個成績");
-        double 成績 = sc.nextDouble();
+        System.out.println("英文成績為？");
+        double 英文 = sc.nextDouble();
+        System.out.println("國文成績為？");
+        double 國文 = sc.nextDouble();
+        System.out.println("數學成績為？");
+        double 數學 = sc.nextDouble();
 
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double C = sc.nextDouble();
-
-        double 總和 = A + B + C;
-        double 平均 = (A + B + C) / 3;
-
-        System.out.println("總和:" +總和 + "平均" + 平均);
+        double 總和 = 英文 + 國文 + 數學;
+        double 平均 = (英文 + 國文 + 數學) / 3;
+        System.out.println("成績總和:" +總和  + "成績平均:" + 平均);
     }
 }
